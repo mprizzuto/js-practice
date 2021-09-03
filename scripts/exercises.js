a4f7e8dc5b60335cece72989962e42cee4b71b31
Marco R <mprizzuto88@gmail.com>
2021-09-03 16:28:00 -0400

    create isAnagram() function

--- scripts/exercises.js
+++ scripts/exercises.js
@@ -1,1 +1,31 @@
-console.log("hllo");
+console.clear();
+
+(function isAnagram(wordOne, wordTwo) {
+    // check for invalid characters
+   let format = /[`~!@#$%^&*()_+{}|? ]+/;
+  // check for numbers
+  wordOne = prompt("type a word");
+    
+  if ( /\d/.test( wordOne ) === true ) {
+    alert("no nums allowe");
+  } else if ( format.test( wordOne ) === true ) {
+      alert("no foreign chars allowed");
+  }
+  else {
+    wordTwo = prompt("type another word");
+    if ( /\d/.test( wordOne ) === true ) {
+      alert("no nums allowe");
+  } else if ( format.test( wordOne ) === true ) {
+      alert("no foreign chars allowed");
+  }
+  else {
+     
+    if (wordOne.length !== wordTwo.length) {
+      alert("words must be same length");
+    } else {
+      alert(`word one is ${wordOne} word two is ${wordTwo}`);
+    }
+  }  
+}
+ 
+}) ();
